dfbc89c2d4a5e98832fc262f009231d7decc2fa9
Evamvid Sharma <evamvid@gmail.com>
2018-09-24 17:08:57 -0400

    add lichess download feature

--- chesscom-games.py
+++ chesscom-games.py
@@ -2,15 +2,23 @@ from codecs import open
 from datetime import date
 import os.path
 import requests
+import shutil
 
-def main(user, file):
+def main(user, file, lichess_api_key):
+    print(lichess_api_key)
     print("Downloading %s's games to %s:" % (user, file))
     try:
         os.remove(file)
     except OSError:
         pass
-    for archive in get('https://api.chess.com/pub/player/%s/games/archives' % user)['archives']:
+    for archive in requests.get('https://api.chess.com/pub/player/%s/games/archives' % user).json()['archives']:
         download_archive(archive, file)
+    print("about to make lichess request")
+    r = requests.get('https://lichess.org/api/games/user/%s' % user, headers={'Authorization': 'Bearer ' + lichess_api_key}, stream=True)
+    print(r.text)
+    with open(file, 'a', encoding = 'utf-8') as output:
+        print(r.text, file=output)
+        print('', file=output)
 
 def download_archive(url, file):
     games = get(url)['games']
@@ -22,14 +30,16 @@ def download_archive(url, file):
             print(game['pgn'], file=output)
             print('', file=output)
 
-def get(url):
-    return requests.get(url).json()
+def get(url, headers=None):
+    return requests.get(url, headers = headers).json()
+
+
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description="Download a user's games from chess.com")
     parser.add_argument('user', metavar='USER', help='The user whose games we want')
-    parser.add_argument('file', metavar='PATH', help='which file (can include directory) to create the PGN files',
-            default="./chessGames.pgn", nargs='?')
+    parser.add_argument('file', metavar='PATH', help='which file (can include directory) to create the PGN files', default="./chessGames.pgn", nargs='?')
+    parser.add_argument('--lichess_api_key', metavar='API_KEY', help =  'the api key for lichess', default=None, nargs = 1)
     args = parser.parse_args()
-    main(args.user, args.file)
+    main(args.user, args.file, args.lichess_api_key[0])
